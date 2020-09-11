a20276bf9aadad50b10a38b9554f3e34e67e604a
dianlujitao <dianlujitao@lineageos.org>
2020-09-11 11:41:51 +0800

    soong: Hack to preserve Eunoia product variables
    Change-Id: Ice4093b86baed6974bab630f0e80f6b13a5e28f3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -1128,6 +1128,11 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
+			if strings.HasPrefix(prefix, "Product_variables.Eunoia") {
+				// Convert Product_variables.Eunoia.Foo to Eunoia.Foo
+				_, prefix = splitPrefix(prefix)
+			}
+
 			// Filter function, returns true if the field should be in the resulting struct
 			if prefix == "" {
 				// Keep the top level Product_variables field
